024d7322135d069d10847dca239d7ce71b46963c
anilyavas <anil.yavas99@gmail.com>
2024-03-27 18:15:37 +0300

    authentication with login signup screens and clerk

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,11 +1,91 @@
-import { View, Text } from 'react-native';
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { Link } from 'expo-router';
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
 const SignUp = () => {
+  const [countryCode, setCountryCode] = useState('+90');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const keyboardVerticalOffset = Platform.OS === 'ios' ? 90 : 0;
+  const onSignup = async () => {};
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior='padding'
+      keyboardVerticalOffset={80}
+    >
+      <View style={defaultStyles.container}>
+        <Text style={defaultStyles.header}>Let's get started!</Text>
+        <Text style={defaultStyles.descriptionText}>
+          Enter your phone number. We will send you a confimation code there
+        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder='Country Code'
+            keyboardType='numeric'
+            placeholderTextColor={Colors.gray}
+            value={countryCode}
+          />
+          <TextInput
+            style={[styles.input, { flex: 1 }]}
+            placeholder='Mobile number'
+            keyboardType='numeric'
+            placeholderTextColor={Colors.gray}
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+          />
+        </View>
+        <Link href={'/login'} asChild>
+          <Pressable>
+            <Text style={defaultStyles.textLink}>
+              Already have an account? Log in
+            </Text>
+          </Pressable>
+        </Link>
+        <View style={{ flex: 1 }} />
+        <Pressable
+          style={[
+            defaultStyles.pillButton,
+            phoneNumber !== '' ? styles.enabled : styles.disabled,
+            { marginBottom: 20 },
+          ]}
+          onPress={onSignup}
+        >
+          <Text style={defaultStyles.buttonText}>Sign up</Text>
+        </Pressable>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 export default SignUp;
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: 'row',
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+});
